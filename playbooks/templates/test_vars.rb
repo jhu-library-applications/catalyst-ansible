d13fe281693bedabd993b3b4675fa0ae76b461e4
dheles <drew@codhicitta.com>
2017-10-09 12:52:45 -0400

    Use variables provided by the TestingEnvironment class

--- playbooks/templates/test_vars.rb
+++ playbooks/templates/test_vars.rb
@@ -1,13 +1,3 @@
-# environment vars: doesn't matter how it gets set
-ENV["CATALYST_URL"]="http://{{ groups['catalyst'][0] }}"
-
-# simple syntax:
-FINDIT_IP="{{ findit_ip }}"
-
-# clearly a global var. global vars are usually bad, though
-$findit_ip = "{{ findit_ip }}"
-
-# encapsulation and abstraction at the cost of complexity
 module TestingEnvironment
   class << self
     attr_accessor :catalyst_url, :findit_ip
